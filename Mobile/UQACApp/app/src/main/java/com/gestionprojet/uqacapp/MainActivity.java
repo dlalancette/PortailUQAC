29b0f128f3abceff91f512bcb15060e079e7d512
Julien Samson <samson.julien.01@gmail.com>
2016-12-08 11:31:47 -0500

    changement menu

--- Mobile/UQACApp/app/src/main/java/com/gestionprojet/uqacapp/MainActivity.java
+++ Mobile/UQACApp/app/src/main/java/com/gestionprojet/uqacapp/MainActivity.java
@@ -1,112 +1,50 @@
 package com.gestionprojet.uqacapp;
 
-        import android.app.Activity;
-        import android.content.res.Configuration;
         import android.os.Bundle;
         import android.support.v4.widget.DrawerLayout;
-        import android.support.v7.app.ActionBarActivity;
         import android.support.v7.app.ActionBarDrawerToggle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.support.v7.widget.Toolbar;
         import android.view.Menu;
         import android.view.MenuItem;
-        import android.view.View;
-        import android.widget.ArrayAdapter;
-        import android.widget.ImageView;
-        import android.widget.ListView;
 
-public class MainActivity extends ActionBarActivity {
-
-    private String[] drawerItemsList;
-    private ListView myDrawer;
-    private String mActivityTitle;
+public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
+    private ActionBarDrawerToggle mToggle;
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mActivityTitle = getTitle().toString();
+        mToolbar = (Toolbar)findViewById(R.id.nav_action);
+        setSupportActionBar(mToolbar);
 
-        //IMPORTANT. Liste des items (nom des liens dans le drawer
-        drawerItemsList = getResources().getStringArray(R.array.items);
-        myDrawer = (ListView) findViewById(R.id.my_drawer);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
 
-        myDrawer.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, drawerItemsList));
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
-
-/*The Navigation Drawer is essentially a list view.
-Create a drawer_item.xml with whatever layout you want (text+imageview) and pass it to the arrayAdapter.
-Then when populating the listView (in the getview method), assign the imageView to the drawable of your choosing.*/
-
-        setupDrawer();
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-    }
-
-    private void setupDrawer() {
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
-
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                super.onDrawerOpened(drawerView);
-                getSupportActionBar().setTitle("UQAC");
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                super.onDrawerClosed(view);
-                getSupportActionBar().setTitle(mActivityTitle);
-                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-            }
-        };
-
-        mDrawerToggle.setDrawerIndicatorEnabled(true);
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        getMenuInflater().inflate(R.menu.menu_action, menu);
         return true;
     }
 
+    //action quelconque lors de la selection d'un item
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //Action when setting is selected
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        //Action when disconnect is selected
-        if (id == R.id.action_disconnect) {
-            return true;
-        }
-
-
-        // Activate the navigation drawer toggle
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
+        //Enable drawer button
+        if(mToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
