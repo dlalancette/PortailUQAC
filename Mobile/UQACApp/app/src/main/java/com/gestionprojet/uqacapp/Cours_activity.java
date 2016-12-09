89ba817459fe7dcbc7579a27122215a81df948d4
pyglidden <pierreyves.glidden@hotmail.com>
2016-12-09 10:01:39 -0500

    ajout page dun cours

---
+++ Mobile/UQACApp/app/src/main/java/com/gestionprojet/uqacapp/Cours_activity.java
@@ -0,0 +1,13 @@
+package com.gestionprojet.uqacapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cours_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cours_activity);
+    }
+}
