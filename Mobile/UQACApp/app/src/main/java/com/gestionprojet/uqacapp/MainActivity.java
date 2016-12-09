1116877726fb40e5dd773b97cbc15ed1c2ac85dd
pyglidden <pierreyves.glidden@hotmail.com>
2016-12-09 10:24:59 -0500

    petites modifications divers

--- Mobile/UQACApp/app/src/main/java/com/gestionprojet/uqacapp/MainActivity.java
+++ Mobile/UQACApp/app/src/main/java/com/gestionprojet/uqacapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gestionprojet.uqacapp;
 
+        import android.content.Intent;
         import android.os.Bundle;
         import android.support.v4.widget.DrawerLayout;
         import android.support.v7.app.ActionBarDrawerToggle;
@@ -7,18 +8,28 @@ package com.gestionprojet.uqacapp;
         import android.support.v7.widget.Toolbar;
         import android.view.Menu;
         import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-
+    private Button btn;
     private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     /*   btn = (Button)findViewById(R.id.btn_connexion);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                startActivity(new Intent(LoginActivity.this, MainActivity.class ))
+            }
+            });*/
 
         mToolbar = (Toolbar)findViewById(R.id.nav_action);
         setSupportActionBar(mToolbar);
